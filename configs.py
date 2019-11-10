ca441e503c259d98010ee93d757600f23a65283e
Omar Mohamed <omar.mohamed.latif@gmail.com>
2019-11-10 21:47:27 +0200

    -added efficientnet support

--- configs.py
+++ configs.py
@@ -15,7 +15,7 @@ class argHandler(dict):
         self.define('image_directory', './IU-XRay/images',
                     'path to folder containing the patient folders which containg the images')
         self.define('visual_model_name', 'DenseNet121',
-                    'select from (VGG16, VGG19, DenseNet121, DenseNet169, DenseNet201, Xception, ResNet50, ResNet101, ResNet152, ResNet50V2, ResNet101V2, ResNet152V2, InceptionV3, InceptionResNetV2, NASNetMobile, NASNetLarge, MobileNet, MobileNetV2). Note that the classifier layer is removed by default.')
+                    'select from (VGG16, VGG19, DenseNet121, DenseNet169, DenseNet201, Xception, ResNet50, ResNet101, ResNet152, ResNet50V2, ResNet101V2, ResNet152V2, InceptionV3, InceptionResNetV2, NASNetMobile, NASNetLarge, MobileNet, MobileNetV2, EfficientNetB0 to EfficientNetB7). Note that the classifier layer is removed by default.')
         self.define('use_chexnet_weights', True,
                     'use pre-trained chexnet weights. Note only works with DenseNet121. If you use this option without popping layers it will have the classifier intact')
         self.define('chexnet_weights_path', 'pretrained_models/chexnet_densenet121_weights.h5', 'chexnet weights path')
@@ -36,7 +36,7 @@ class argHandler(dict):
         self.define('final_layer_pooling', 'avg', 'the pooling to be used as a final layer to the visual model')
         self.define('load_model_path', '',
                     'a path containing the checkpoints. If provided with load_model_name the system will continue the training from that point or use it in testing.')
-        self.define('save_model_path', 'DenseNet121_17',
+        self.define('save_model_path', 'saved_model',
                     'where to save the checkpoints. The path will be created if it does not exist. The system saves every epoch by default')
         self.define('save_best_model_only', True,
                     'Only save the best weights on validation loss')
@@ -51,10 +51,10 @@ class argHandler(dict):
         self.define('generator_workers', 4, 'The number of cpu workers generating batches.')
         self.define('generator_queue_length', 12, 'The maximum number of batches in the queue to be trained on.')
         self.define('minimum_learning_rate', 1e-7, 'The minimum possible learning rate when decaying')
-        self.define('reduce_lr_patience', 3,
+        self.define('reduce_lr_patience', 4,
                     'The number of epochs to reduce the learning rate when validation loss is not decreasing')
         self.define('show_model_summary', True, 'A flag to show or hide the model summary')
-        self.define('positive_weights_multiply', 20.0, 'Controls the class_weight ratio between 0 and 1. Higher value means higher weighting of positive samples. Only works if use_class_balancing is set to true')
+        self.define('positive_weights_multiply', 1.0, 'Controls the class_weight ratio between 0 and 1. Higher value means higher weighting of positive samples. Only works if use_class_balancing is set to true')
         self.define('use_class_balancing', True, 'If set to true it will automatically balance the classes by settings class weights')
 
     def define(self, argName, default, description):
